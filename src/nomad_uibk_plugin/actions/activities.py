9755500eb108d0aaf828498b214d55aa140aec5a
GinzburgLev <ginzburglev95@gmail.com>
2025-11-27 14:50:32 +0100

    linting

--- src/nomad_uibk_plugin/actions/activities.py
+++ src/nomad_uibk_plugin/actions/activities.py
@@ -13,7 +13,7 @@ from nomad.datamodel import User
 from nomad.datamodel.metainfo.plot import PlotlyFigure
 from nomad.processing.data import Entry
 from PIL import Image
-from skimage import color, feature, io, transform
+from skimage import color, feature, io
 from skimage.transform import hough_circle, hough_circle_peaks
 from temporalio import activity
 
@@ -29,10 +29,11 @@ from nomad_uibk_plugin.schema_packages.IFMschema import (
 )
 from nomad_uibk_plugin.schema_packages.sample import UIBKSampleReference
 
-Image.MAX_IMAGE_PIXELS = 1000000000     #increases limit for image size
+Image.MAX_IMAGE_PIXELS = 1000000000  # increases limit for image size
 
 PATCH_SIZE = 640
-LONG_SIDE_PLOT = 512    # approximate value due to int operations
+LONG_SIDE_PLOT = 512  # approximate value due to int operations
+
 
 @activity.defn
 def mask_image(masking_data: MaskingInput):
@@ -134,7 +135,7 @@ def run_ifm_inference(data: ActivityInferenceInput):
                 os.remove(data.image_file_name)
             except Exception as e:
                 activity.logger.warning(
-                    f'Could not remove temporary masked image file {data.image_file_name}: {e}'
+                    f'Could not remove temporary file {data.image_file_name}: {e}'
                 )
     else:
         activity.logger.info('Output file already exists and not overwritten.')
@@ -148,23 +149,25 @@ async def read_file_and_write_archive(writer_input: WriteArchiveInput):  # noqa:
         raise FileExistsError('No h5 file found.')
 
     with h5py.File(writer_input.h5_path, 'r') as f:
-        mask = f["mask"][()] # type: ignore
-        mask_types = f["mask_layers"][()] # type: ignore
-    
+        mask = f['mask'][()]  # type: ignore
+        mask_types = f['mask_layers'][()]  # type: ignore
+
     # count defect prevalence from the masks
-    mask_types = [msk.decode() for msk in mask_types] # type: ignore
-    defect_pixels = np.sum(mask, axis=(1, 2)) # type: ignore
+    mask_types = [msk.decode() for msk in mask_types]  # type: ignore
+    defect_pixels = np.sum(mask, axis=(1, 2))  # type: ignore
     defect_types = mask_types
     no_def_pixel = 0
-    for i, mask_type, def_pixel in zip(range(len(mask_types)), mask_types, defect_pixels):
-        if mask_type == "Sample":
+    for i, mask_type, def_pixel in zip(
+        range(len(mask_types)), mask_types, defect_pixels
+    ):
+        if mask_type == 'Sample':
             total_pixel = def_pixel
             no_def_pixel += def_pixel
             no_def_index = i
         else:
             no_def_pixel -= def_pixel
 
-    defect_types[no_def_index] = "No Defect"
+    defect_types[no_def_index] = 'No Defect'
     relative_share = defect_pixels / total_pixel
     relative_share[no_def_index] = no_def_pixel / total_pixel
 
@@ -173,7 +176,7 @@ async def read_file_and_write_archive(writer_input: WriteArchiveInput):  # noqa:
         defect_prevalence.append(DefectPrevalence(name=key, prevalence=value))
 
     # Downscale masks for plotting
-    layers, h, w = mask.shape # type: ignore
+    layers, h, w = mask.shape  # type: ignore
     long_side = max(h, w)
     scale = max(long_side // LONG_SIDE_PLOT, 1)
     small_h = h // scale
@@ -185,14 +188,16 @@ async def read_file_and_write_archive(writer_input: WriteArchiveInput):  # noqa:
         row_in_buffer = 0
         out_row = 0
         for row_index in range(h):
-            row_buffer[row_in_buffer] = mask[layer, row_index] # type: ignore
+            row_buffer[row_in_buffer] = mask[layer, row_index]  # type: ignore
             row_in_buffer += 1
 
             # enough raws in buffer for one pool vertically
             if row_in_buffer == scale:
                 # pool vertically, then horizontally
                 v_pool = row_buffer.max(axis=0)
-                h_and_v_pool = v_pool[:small_w*scale].reshape(small_w, scale).max(axis=1)
+                h_and_v_pool = (
+                    v_pool[: small_w * scale].reshape(small_w, scale).max(axis=1)
+                )
                 small_mask[layer, out_row] = h_and_v_pool
                 out_row += 1
                 row_in_buffer = 0
@@ -201,20 +206,24 @@ async def read_file_and_write_archive(writer_input: WriteArchiveInput):  # noqa:
     data_to_show = np.zeros((small_h, small_w), dtype=int)
     for i in range(layers):
         if i != no_def_index:
-            data_to_show += (np.logical_not(data_to_show) & small_mask[i]).astype(int)*(i+1)
-    data_to_show += (np.logical_not(data_to_show) & small_mask[no_def_index]).astype(int)*(no_def_index+1)
-    h_mm = h*writer_input.pixel_size*1000
-    w_mm = w*writer_input.pixel_size*1000
+            data_to_show += (np.logical_not(data_to_show) & small_mask[i]).astype(
+                int
+            ) * (i + 1)
+    data_to_show += (np.logical_not(data_to_show) & small_mask[no_def_index]).astype(
+        int
+    ) * (no_def_index + 1)
+    h_mm = h * writer_input.pixel_size * 1000
+    w_mm = w * writer_input.pixel_size * 1000
     # create defects figure
     heatmap = go.Heatmap(
-        x0 = 0,
-        dx = w_mm / small_w,
-        y0 = 0,
-        dy = h_mm / small_h,
+        x0=0,
+        dx=w_mm / small_w,
+        y0=0,
+        dy=h_mm / small_h,
         z=data_to_show[-1:0:-1],
         colorscale='Blackbody',
         colorbar=dict(
-            tickvals=list(range(1,len(defect_types)+1)),
+            tickvals=list(range(1, len(defect_types) + 1)),
             ticktext=defect_types,
             title='Defect Type',
         ),
@@ -294,10 +303,10 @@ async def process_new_files(data: ProcessNewFilesInput):
         only_updated_files=True,
     )
 
-    await handle.result() # type: ignore
+    await handle.result()  # type: ignore
 
     result_entry_refs = []
-    all_entries_this_upload = Entry.objects(upload_id=upload.upload_id) # type: ignore
+    all_entries_this_upload = Entry.objects(upload_id=upload.upload_id)  # type: ignore
 
     for mainfile_name in mainfile_names:
         for entry in all_entries_this_upload:
