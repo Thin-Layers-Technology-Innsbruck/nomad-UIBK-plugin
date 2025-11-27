fix figure, fix excesive RAM use in masking, delete temporal masked image after running inference, update timeouts

src/nomad_uibk_plugin/actions/activities.py
@@ -6,14 +6,13 @@ import time
 import h5py
 import imageio
 import numpy as np
-import pandas as pd
-import plotly.express as px
 import plotly.graph_objs as go
 from ifm_imagesegmentation.inference import run_image_segmentation
 from nomad.app.v1.routers.uploads import get_upload_with_read_access
 from nomad.datamodel import User
 from nomad.datamodel.metainfo.plot import PlotlyFigure
 from nomad.processing.data import Entry
+from PIL import Image
 from skimage import color, feature, io, transform
 from skimage.transform import hough_circle, hough_circle_peaks
 from temporalio import activity
@@ -29,6 +28,8 @@ from nomad_uibk_plugin.schema_packages.IFMschema import (
     IFMTwoStepAnalysisResult,
 )
 from nomad_uibk_plugin.schema_packages.sample import UIBKSampleReference
+
+Image.MAX_IMAGE_PIXELS = 1000000000     #increases limit for image size
 
 PATCH_SIZE = 640
 LONG_SIDE_PLOT = 512    # approximate value due to int operations
@@ -50,14 +51,12 @@ def mask_image(masking_data: MaskingInput):
                 raise FileExistsError(f'Could not read {input_path}')
             h, w = img.shape[:2]
             # Reduce resolution and convert to grayscale for faster image analysis
+            # Use PIL resizing to save on RAM
             scale = resize_side / min(h, w)
             small_w, small_h = int(w * scale), int(h * scale)
-            small_img = transform.resize(
-                img,
-                (small_h, small_w),
-                anti_aliasing=False,
-                preserve_range=True,
-            ).astype(np.uint8)  # type: ignore
+            pil_img = Image.fromarray(img)
+            pil_small = pil_img.resize((small_w, small_h), Image.Resampling.LANCZOS)
+            small_img = np.asarray(pil_small, dtype=np.uint8)
             gray_small = color.rgb2gray(small_img)
             # Edge detection
             edges = feature.canny(gray_small, sigma=5)
@@ -80,11 +79,24 @@ def mask_image(masking_data: MaskingInput):
             x = int(round((x_s + 1) * scale_back))
             y = int(round((y_s + 1) * scale_back))
             r = int(round(r_s * scale_back))
-            # Create and apply the mask
-            Y_grid, X_grid = np.ogrid[:h, :w]
-            dist2 = (X_grid - x) ** 2 + (Y_grid - y) ** 2
-            mask = dist2 <= r**2
-            img[~mask] = 0
+            # Create and apply the mask - row by row to save on RAM
+            r2 = r * r
+            for yi in range(h):
+                dy = yi - y
+                dy2 = dy * dy
+                if dy2 > r2:
+                    img[yi, :] = 0
+                    continue
+
+                dx = int((r2 - dy2) ** 0.5)
+                x_start = max(0, x - dx)
+                x_end = min(w, x + dx)
+
+                if x_start > 0:
+                    img[yi, :x_start] = 0
+
+                if x_end < w:
+                    img[yi, x_end:] = 0
             # Save the result
             imageio.imwrite(output_path, img.astype(np.uint8))
         else:
@@ -116,24 +128,32 @@ def run_ifm_inference(data: ActivityInferenceInput):
             save_png=data.save_resulting_image,
             save_h5=True,
         )
+        # delete the temporary masked file
+        if data.mask_input_images:
+            try:
+                os.remove(data.image_file_name)
+            except Exception as e:
+                activity.logger.warning(
+                    f'Could not remove temporary masked image file {data.image_file_name}: {e}'
+                )
     else:
         activity.logger.info('Output file already exists and not overwritten.')
 
 
 @activity.defn
-async def read_file_and_write_archive(writer_input: WriteArchiveInput):
+async def read_file_and_write_archive(writer_input: WriteArchiveInput):  # noqa: PLR0915
     if not os.path.exists(writer_input.csv_path):
         raise FileExistsError('No csv file found.')
     if not os.path.exists(writer_input.h5_path):
         raise FileExistsError('No h5 file found.')
 
     with h5py.File(writer_input.h5_path, 'r') as f:
-        mask = f["mask"][()]
-        mask_types = f["mask_layers"][()]
+        mask = f["mask"][()] # type: ignore
+        mask_types = f["mask_layers"][()] # type: ignore
     
     # count defect prevalence from the masks
-    mask_types = [msk.decode() for msk in mask_types]
-    defect_pixels = np.sum(mask, axis=(1, 2))
+    mask_types = [msk.decode() for msk in mask_types] # type: ignore
+    defect_pixels = np.sum(mask, axis=(1, 2)) # type: ignore
     defect_types = mask_types
     no_def_pixel = 0
     for i, mask_type, def_pixel in zip(range(len(mask_types)), mask_types, defect_pixels):
@@ -165,7 +185,7 @@ async def read_file_and_write_archive(writer_input: WriteArchiveInput):
         row_in_buffer = 0
         out_row = 0
         for row_index in range(h):
-            row_buffer[row_in_buffer] = mask[layer, row_index]
+            row_buffer[row_in_buffer] = mask[layer, row_index] # type: ignore
             row_in_buffer += 1
 
             # enough raws in buffer for one pool vertically
@@ -182,12 +202,33 @@ async def read_file_and_write_archive(writer_input: WriteArchiveInput):
     for i in range(layers):
         if i != no_def_index:
             data_to_show += (np.logical_not(data_to_show) & small_mask[i]).astype(int)*(i+1)
-    data_to_show += small_mask[no_def_index].astype(int)
-    figure = px.imshow(
-        data_to_show,
-        # color_continuous_scale=["black", "white"]
+    data_to_show += (np.logical_not(data_to_show) & small_mask[no_def_index]).astype(int)*(no_def_index+1)
+    h_mm = h*writer_input.pixel_size*1000
+    w_mm = w*writer_input.pixel_size*1000
+    # create defects figure
+    heatmap = go.Heatmap(
+        x0 = 0,
+        dx = w_mm / small_w,
+        y0 = 0,
+        dy = h_mm / small_h,
+        z=data_to_show[-1:0:-1],
+        colorscale='Blackbody',
+        colorbar=dict(
+            tickvals=list(range(1,len(defect_types)+1)),
+            ticktext=defect_types,
+            title='Defect Type',
+        ),
     )
-    figure.update_coloraxes(showscale=False)
+
+    figure = go.Figure(data=heatmap)
+    figure.update_layout(
+        title='Heatmap of Defect Distribution',
+        xaxis_title='X Position [mm]',
+        yaxis_title='Y Position [mm]',
+        xaxis=dict(scaleanchor='y'),
+        yaxis=dict(scaleanchor='x'),
+        autosize=True,
+    )
     figure_json = figure.to_plotly_json()
 
     # create a new archive entry with the results of the analysis
@@ -218,81 +259,6 @@ async def read_file_and_write_archive(writer_input: WriteArchiveInput):
         json.dump({'data': result_entry.m_to_dict(with_root_def=True)}, f, indent=4)
 
     return fname
-
-# @activity.defn
-# async def read_file_and_write_archive(writer_input: WriteArchiveInput):
-#     if not os.path.exists(writer_input.csv_path):
-#         raise FileExistsError('No csv file found.')
-#     else:
-#         defect_data = pd.read_csv(writer_input.csv_path, skiprows=2)
-#         defect_columns = defect_data.columns.to_list()
-#         defect_columns.remove('x')
-#         defect_columns.remove('y')
-#         defect_data['type'] = defect_data[defect_columns].idxmax(axis=1)
-#         relative_share = defect_data['type'].value_counts(normalize=True)
-#         relative_share = relative_share.reindex(defect_columns, fill_value=0.0)
-#         relative_share = relative_share.sort_values(ascending=False)
-#         defect_mapping = {key: idx for idx, key in enumerate(defect_columns, start=1)}
-#         defect_data['label'] = defect_data['type'].map(defect_mapping)
-
-#     defect_prevalence = []
-#     for key, value in relative_share.items():
-#         defect_prevalence.append(DefectPrevalence(name=key, prevalence=value))
-
-#     # create defects figure
-#     heatmap = go.Heatmap(
-#         x=defect_data['x'],
-#         y=defect_data['y'],
-#         z=defect_data['label'],
-#         colorscale='Viridis',
-#         colorbar=dict(
-#             tickvals=[1, 2, 3, 4],
-#             ticktext=defect_columns,
-#             title='Defect Type',
-#         ),
-#     )
-
-#     figure = go.Figure(data=heatmap)
-#     figure.update_layout(
-#         title='Heatmap of Defect Distribution',
-#         xaxis_title='X Position',
-#         yaxis_title='Y Position',
-#         xaxis=dict(scaleanchor='y'),
-#         yaxis=dict(scaleanchor='x'),
-#         autosize=True,
-#     )
-
-#     figure_json = figure.to_plotly_json()
-#     figure_json['config'] = {'staticPlot': True}
-
-#     # create a new archive entry with the results of the analysis
-#     result_name = (
-#         re.sub(r'_prediction\.csv$', '', writer_input.csv_path) + '_inference_result'
-#     )
-#     activity_info = activity.info()
-
-#     result_entry = IFMTwoStepAnalysisResult(
-#         name=result_name.split('/')[-1],
-#         file=writer_input.csv_path,
-#         defect_prevalence=defect_prevalence,
-#         action_id=activity_info.workflow_id,
-#         image_masked=writer_input.mask_input_images,
-#         sample=UIBKSampleReference(lab_id=writer_input.sample_id),
-#         figures=[
-#             PlotlyFigure(
-#                 label='Defect Distribution Heatmap',
-#                 index=0,
-#                 figure=figure_json,
-#             )
-#         ],
-#     )
-
-#     # add the new .archive.json entry to the upload
-#     fname = os.path.join(result_name + '.archive.json')
-#     with open(fname, 'w', encoding='utf-8') as f:
-#         json.dump({'data': result_entry.m_to_dict(with_root_def=True)}, f, indent=4)
-
-#     return fname
 
 
 @activity.defn
@@ -328,10 +294,10 @@ async def process_new_files(data: ProcessNewFilesInput):
         only_updated_files=True,
     )
 
-    await handle.result()
+    await handle.result() # type: ignore
 
     result_entry_refs = []
-    all_entries_this_upload = Entry.objects(upload_id=upload.upload_id)
+    all_entries_this_upload = Entry.objects(upload_id=upload.upload_id) # type: ignore
 
     for mainfile_name in mainfile_names:
         for entry in all_entries_this_upload: